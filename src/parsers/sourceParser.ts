791e831e3affec9ed189fccedc7cd9c3b80bd71a
そめか <someka.vrc@gmail.com>
2026-02-08 10:41:48 +0900

    reset

--- src/parsers/sourceParser.ts
+++
@@ -1,6 +0,0 @@
-import { LocalizationCall } from "./types";
-
-export interface SourceParser {
-  findAllLocalizationCalls(text: string, funcs?: string[]): LocalizationCall[];
-  findLocalizationCallAtOffset(text: string, offset: number, funcs?: string[]): LocalizationCall | null;
-}
--- src/parsers/types.ts
+++
@@ -1,8 +0,0 @@
-export type LocalizationCall = {
-  msgid: string;
-  start: number;
-  end: number;
-  callStart: number;
-  callEnd: number;
-  funcName: string;
-};
